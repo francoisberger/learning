12c3a5c36c42742ee786b0923e78fe7211d2e0ba
francoisberger <francoisberger.dev@gmail.com>
2022-04-16 22:42:00 +0200

    Modified versions for modules + added the java-tests module.

---
+++ java-tests/src/main/java/io/github/francoisberger/javatests/App.java
@@ -0,0 +1,13 @@
+package io.github.francoisberger.javatests;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
